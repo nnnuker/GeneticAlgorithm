8f24b20baab6f665aa035d9ac60b5101d77d5b68
Valery Larkovich <nnoookk@gmail.com>
2016-03-31 00:30:58 +0300

    Added tests for GetValue.

--- TestsGA/ChromosomeTest.cs
+++ TestsGA/ChromosomeTest.cs
@@ -38,5 +38,16 @@ namespace TestsGA
             Chromosome chromosome = new Chromosome(0.1, -35, 4, 5, "X");
             Assert.AreEqual("10001010000", chromosome.Binary);
         }
+
+        [TestMethod]
+        public void ConvertToDouble()
+        {
+            Chromosome chromosome = new Chromosome(0.1, -12315, 534846, 42424, "X");
+            Chromosome chromosome1 = new Chromosome(0.1, -35, 4, -5.5, "X");
+            Chromosome chromosome2 = new Chromosome(0.1, -4, 4, 5.5, "X");
+            Assert.AreEqual(42424, chromosome.GetValue());
+            Assert.AreEqual(-5.5, chromosome1.GetValue());
+            Assert.AreEqual(5.5, chromosome2.GetValue());
+        }
     }
 }
