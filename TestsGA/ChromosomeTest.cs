8264de87fec54a70c9c482955f2f8f8cf2d059b8
Valery Larkovich <nnoookk@gmail.com>
2016-03-30 23:51:25 +0300

    Added chromosome tests.

---
+++ TestsGA/ChromosomeTest.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GeneticAlgorithm.Chromosome;
+
+namespace TestsGA
+{
+    [TestClass]
+    public class ChromosomeTest
+    {
+        [TestMethod]
+        public void BinaryFormat()
+        {
+            Chromosome chromosome = new Chromosome(0.1, -4, 4, 5.5, "X");
+            Assert.AreEqual("101010101", chromosome.Binary);
+        }
+
+        [TestMethod]
+        public void BinaryFormatMinus()
+        {
+            Chromosome chromosome = new Chromosome(0.1, -4, 4, -5.5, "X");
+            Assert.AreEqual("001010101", chromosome.Binary);
+        }
+
+        [TestMethod]
+        public void BigNumber()
+        {
+            Chromosome chromosome = new Chromosome(0.1, -35, 4, -5.5, "X");
+            Assert.AreEqual("00001010101", chromosome.Binary);
+        }
+
+        [TestMethod]
+        public void Integer()
+        {
+            Chromosome chromosome = new Chromosome(0.1, -35, 4, 5, "X");
+            Assert.AreEqual("10001010000", chromosome.Binary);
+        }
+    }
+}
