7691e2350b6441d8164cde3e239d702695ff804d
otemko <otemko@yandex.ru>
2016-03-30 23:44:15 +0300

    Change RandomPairFormation and PairFormationTest

--- GeneticAlgorithm/PairFormation/Pair.cs
+++ GeneticAlgorithm/PairFormation/Pair.cs
@@ -4,7 +4,9 @@ namespace GeneticAlgorithm.PairFormation
 {
     public class Pair : IPair
     {
+        #region Property
         public IDesignPoint First { get; set; }
         public IDesignPoint Second { get; set; }
+        #endregion  
     }
 }
--- TestsGA/PairFormationTest.cs
+++ TestsGA/PairFormationTest.cs
@@ -4,6 +4,7 @@ using GeneticAlgorithm.PairFormation;
 using GeneticAlgorithm.DesignPoints;
 using GeneticAlgorithm.Chromosome;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace TestsGA
 {
@@ -11,19 +12,51 @@ namespace TestsGA
     public class PairFormationTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void RandomPairFormationCountPairs()
         {
-            var pp = new RandomPairFormation();
+            var randomPF = new RandomPairFormation();
 
-            List<DesignPoint> dd = new List<DesignPoint>();
-            Chromosome hr = new Chromosome(1, 1, 1, "");
-            DesignPoint dss = new DesignPoint(1, 1, hr);
-            dd.Add(new DesignPoint(1, 1, new Chromosome(1, 1, 1, "s")));
-            dd.Add(new DesignPoint(1, 2, new Chromosome(1, 1, 1, "s")));
-            dd.Add(new DesignPoint(1, 3, new Chromosome(1, 1, 1, "s")));
+            List<DesignPoint> listDesignPoint = new List<DesignPoint>();
 
-            List<Pair> list = (List<Pair>)pp.FormatPairs(dd);
-            Assert.AreEqual(list.Count, 2);
+            listDesignPoint.Add(new DesignPoint(1, 0, new Chromosome(1, 1, 1, "s")));
+            listDesignPoint.Add(new DesignPoint(1, 1, new Chromosome(1, 1, 1, "s")));
+            listDesignPoint.Add(new DesignPoint(1, 2, new Chromosome(1, 1, 1, "s")));
+            listDesignPoint.Add(new DesignPoint(1, 3, new Chromosome(1, 1, 1, "s")));
+            listDesignPoint.Add(new DesignPoint(1, 4, new Chromosome(1, 1, 1, "s")));
+
+            IEnumerable<IPair> listResult = randomPF.FormatPairs(listDesignPoint);
+            Assert.AreEqual(listResult.Count(), 3, "Count pairs not equal");
+        }
+
+        [TestMethod]
+        public void RandomPairFormationConsistDesignPoints()
+        {
+            var randomPF = new RandomPairFormation();
+
+            List<DesignPoint> listDesignPoint = new List<DesignPoint>();
+
+            listDesignPoint.Add(new DesignPoint(1, 0, new Chromosome(1, 1, 1, "s")));
+            listDesignPoint.Add(new DesignPoint(1, 1, new Chromosome(1, 1, 1, "s")));
+            listDesignPoint.Add(new DesignPoint(1, 2, new Chromosome(1, 1, 1, "s")));
+            listDesignPoint.Add(new DesignPoint(1, 3, new Chromosome(1, 1, 1, "s")));
+            listDesignPoint.Add(new DesignPoint(1, 4, new Chromosome(1, 1, 1, "s")));
+
+            IEnumerable<IPair> listResult = randomPF.FormatPairs(listDesignPoint);
+
+            int numberConsist = 0;
+
+            foreach (var itemListResult in listResult)
+            {
+                foreach (var itemListDesignPoint in listDesignPoint)
+                {
+                    if (itemListResult.First == itemListDesignPoint || itemListResult.Second == itemListDesignPoint)
+                    {
+                        numberConsist++;
+                    }
+                }
+            }
+
+            Assert.AreEqual(numberConsist, listDesignPoint.Count, "Count numer consist not equal");
         }
     }
 }
