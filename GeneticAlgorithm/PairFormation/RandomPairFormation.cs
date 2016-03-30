80e003f945ed52ad77692fe5302cc74a0e258168
otemko <otemko@yandex.ru>
2016-03-30 21:28:41 +0300

    Add RandomPairFormation for Pairs

--- GeneticAlgorithm/PairFormation/RandomPairFormation.cs
+++ GeneticAlgorithm/PairFormation/RandomPairFormation.cs
@@ -1,17 +1,71 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using GeneticAlgorithm.DesignPoints;
+using System.Linq;
+using System;
 
 namespace GeneticAlgorithm.PairFormation
 {
     public class RandomPairFormation : IPairFormation
     {
-        public IEnumerable<IPair> FormatPairs(IEnumerable<DesignPoint> designPoints)
+        Random random;
+        List<int> usedNumbers;
+
+        public RandomPairFormation()
         {
-            throw new NotImplementedException();
+            random = new Random(DateTime.Now.Millisecond);
+            usedNumbers = new List<int>();
+        }
+
+        public IEnumerable<IPair> FormatPairs(IEnumerable<IDesignPoint> designPoints)
+        {
+            var pairs = new List<IPair>();
+            var count = designPoints.Count();
+
+            int first, second;
+
+            for (int i = 0; i < count/2; i++)
+            {
+                do
+                {
+                    first = random.Next(count);
+                }
+                while (CheckNumber(first));
+
+                usedNumbers.Add(first);
+
+                do
+                {
+                    second = random.Next(count);
+                }
+                while (CheckNumber(second));
+
+                usedNumbers.Add(second);
+                pairs.Add(new Pair { First = designPoints.ElementAt(first), Second = designPoints.ElementAt(second) });
+            }
+            
+            if (count % 2 != 0)
+            {
+                var i = 0;
+
+                foreach (var usedNumber in usedNumbers)
+                {
+                   if (i != usedNumber)
+                        pairs.Add(new Pair { First = designPoints.ElementAt(i), Second = null });
+                }
+            }
+
+            return pairs;
+
+        }
+
+        private bool CheckNumber(int number)
+        {
+            foreach (var usedNumber in usedNumbers)
+            {
+                if (number == usedNumber)
+                    return false;
+            }
+            return true;
         }
     }
 }
--- PresentationForms/MainForm.cs
+++ PresentationForms/MainForm.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using 
 
 namespace PresentationForms
 {
@@ -15,6 +16,7 @@ namespace PresentationForms
         public MainForm()
         {
             InitializeComponent();
+
         }
 
         private void contextMenuStrip1_Opening(object sender, CancelEventArgs e)
