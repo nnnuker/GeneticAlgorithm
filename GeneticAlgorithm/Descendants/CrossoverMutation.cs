d6947f102b262e4d24a3972804109077d55e951d
Valery Larkovich <nnoookk@gmail.com>
2016-04-15 01:27:07 +0300

    Implemented descendants.

--- GeneticAlgorithm/Descendants/CrossoverMutation.cs
+++ GeneticAlgorithm/Descendants/CrossoverMutation.cs
@@ -5,14 +5,39 @@ using System.Text;
 using System.Threading.Tasks;
 using GeneticAlgorithm.Crossover;
 using GeneticAlgorithm.DesignPoints;
+using GeneticAlgorithm.Mutation;
+using GeneticAlgorithm.PairFormation;
 
 namespace GeneticAlgorithm.Descendants
 {
     public class CrossoverMutation : IDescendants
     {
-        public IEnumerable<IDesignPoint> GetDescendants(ICrossover crossover, double mutationCoeff)
+        private readonly ICrossover crossover;
+        private readonly IMutation mutation;
+        private readonly IPairFormation pairFormation;
+
+        public CrossoverMutation()
         {
-            throw new NotImplementedException();
+            
+        }
+
+        public CrossoverMutation(ICrossover crossover, IMutation mutation, IPairFormation pairFormation)
+        {
+            if (crossover == null) throw new ArgumentNullException(nameof(crossover));
+            if (mutation == null) throw new ArgumentNullException(nameof(mutation));
+            if (pairFormation == null) throw new ArgumentNullException(nameof(pairFormation));
+            this.crossover = crossover;
+            this.mutation = mutation;
+            this.pairFormation = pairFormation;
+        }
+
+        public IEnumerable<IDesignPoint> GetDescendants(IEnumerable<IDesignPoint> designPoints)
+        {
+            var pairs = pairFormation.FormatPairs(designPoints);
+
+            var crossovered = crossover.Crossover(pairs);
+
+            return mutation.Mutate(crossovered);
         }
     }
 }
--- GeneticAlgorithm/Descendants/IDescendants.cs
+++ GeneticAlgorithm/Descendants/IDescendants.cs
@@ -1,15 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using GeneticAlgorithm.DesignPoints;
-using GeneticAlgorithm.Crossover;
 
 namespace GeneticAlgorithm.Descendants
 {
     public interface IDescendants
     {
-        IEnumerable<IDesignPoint> GetDescendants(ICrossover crossover, double mutationCoeff);
+        IEnumerable<IDesignPoint> GetDescendants(IEnumerable<IDesignPoint> designPoints); 
     }
 }
