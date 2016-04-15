6fc63e73b2c2e6f85a1dd2f9c7f1a7fb0d364c82
otemko <otemko@yandex.ru>
2016-04-15 21:03:36 +0300

    Add Mutation and MutationTest

--- GeneticAlgorithm/Mutation/MutationBinary.cs
+++ GeneticAlgorithm/Mutation/MutationBinary.cs
@@ -1,11 +1,14 @@
 ﻿using System.Collections.Generic;
 using GeneticAlgorithm.DesignPoints;
+using System;
+using GeneticAlgorithm.Chromosome;
 
 namespace GeneticAlgorithm.Mutation
 {
     public class MutationBinary : IMutation
     {
         private double mutationCoefficient;
+        private Random rnd;
 
         public MutationBinary() : this(0)
         {
@@ -15,11 +18,52 @@ namespace GeneticAlgorithm.Mutation
         public MutationBinary(double mutationCoefficient)
         {
             this.mutationCoefficient = mutationCoefficient;
+            rnd = new Random(DateTime.Today.Millisecond);
         }
 
         public IEnumerable<IDesignPoint> Mutate(IEnumerable<IDesignPoint> designPoints)
         {
-            throw new System.NotImplementedException();
+            if (designPoints == null)
+                throw new ArgumentException();
+
+            var maxValueRnd = 101;
+
+            var result = new List<IDesignPoint>();
+
+            foreach (var itemDP in designPoints)
+            {
+                var newDesignPoint = itemDP.Clone();
+                var newListChromosome = new List<IChromosome>();
+
+                foreach (var itemX in newDesignPoint.X)
+                {
+                    var newChomosome = itemX.Clone();
+                    var oldBinary = newChomosome.Binary;
+                    var newBinary = new List<byte>();
+
+                    foreach (var itemOldBinary in oldBinary)
+                    {
+                        var comparerCoefficient = rnd.Next(0, maxValueRnd);
+
+                        if (comparerCoefficient <= mutationCoefficient)
+                        {
+                            if (itemOldBinary == 0)
+                                newBinary.Add(1);
+                            else
+                                newBinary.Add(0);
+                        }
+                        else
+                        {
+                            newBinary.Add(itemOldBinary);
+                        }
+                    }
+                    newChomosome.Update(newBinary);
+                    newListChromosome.Add(newChomosome);
+                }
+                newDesignPoint.X = newListChromosome;
+                result.Add(newDesignPoint);
+            }
+            return result;
         }
     }
 }
---
+++ TestsGA/MutationTest.cs
@@ -0,0 +1,30 @@
+﻿using GeneticAlgorithm.Chromosome;
+using GeneticAlgorithm.DesignPoints;
+using GeneticAlgorithm.FuncCalculator;
+using GeneticAlgorithm.Mutation;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestsGA
+{
+    [TestClass]
+    public class MutationTest
+    {
+        [TestMethod]
+        public void MutationCoount()
+        {
+            var listDP = new List<IDesignPoint>();
+            listDP.Add(new DesignPoint(1, new FuncCalculatorBasic("X+Y"), new Chromosome(0, 10, 20, 15, "X"), new Chromosome(0, 5, 20, 10, "Y")));
+            listDP.Add(new DesignPoint(1, new FuncCalculatorBasic("X+Y"), new Chromosome(0, 10, 30, 20, "X"), new Chromosome(0, 5, 20, 20, "Y")));
+            listDP.Add(new DesignPoint(1, new FuncCalculatorBasic("X+Y"), new Chromosome(0, 10, 30, 15, "X"), new Chromosome(0, 5, 20, 15, "Y")));
+            var mutation = new MutationBinary(50);
+
+            Assert.AreEqual(mutation.Mutate(listDP).Count(), 3);
+
+        }
+    }
+}
